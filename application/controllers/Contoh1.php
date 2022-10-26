a9d86d46324e6ab37741bcda56f570fb77ca59bf
rizkykhairullah <rizkykhairullah19@gmail.com>
2022-10-27 00:02:18 +0700

    Pencocokan dengan modul

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,10 +1,11 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Contoh1 extends CI_Controller
-{
-    public function index()
-    {
-        echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Muhammad Rizky Khairullah saya tinggal di daerah Bogor, saya suka coding";
-    }
+class Contoh1 extends CI_Controller {
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+        echo "<h1>Welcome to Controller <span>Contoh1</span></h1>";
+        echo "<style>span {color: blue}</style>";
+	}
 }
