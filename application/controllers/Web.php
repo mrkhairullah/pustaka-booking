97202c650747b313ee06fe220c4e6e70be1d4c94
rizkykhairullah <rizkykhairullah19@gmail.com>
2022-10-27 00:24:49 +0700

    Update file

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -3,11 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Web extends CI_Controller
 {
-    function __construct()
-    {
-        parent::__construct();
-        $this->load->helper('url');
-    }
+    // function __construct()
+    // {
+    //     parent::__construct();
+    //     $this->load->helper('url');
+    // }
     public function index()
     {
         $data['judul'] = "Halaman Depan";
